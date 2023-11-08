88438a484b0bd5b5f727e5660ba86206a0a16577
Çagatay Üresin <cagatay.uresin@detaysoft.com>
2023-11-08 11:52:28 +0300

    removed curl installation which is already we have

--- ff_fabric_setup.sh
+++ ff_fabric_setup.sh
@@ -32,7 +32,7 @@ sudo apt install -y git jq wget
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Install Fresh Docker and Docker Compose
 # Add Docker's official GPG key:
-sudo apt install -y ca-certificates curl gnupg
+sudo apt install -y ca-certificates gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
